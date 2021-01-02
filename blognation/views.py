25301c273d872ac79dce3da7aba5f24c70797205
rodincode <rodincode@gmail.com>
2021-01-02 12:53:30 +0530

    added Home

--- blognation/views.py
+++ blognation/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,request
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'blog/home.html', {})
